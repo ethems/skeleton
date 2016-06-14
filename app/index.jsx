60802937a0d7b04ddadf0cea59016de9ba79aa71
furkano <Furkan.Ozdeslik@confirmit.com>
2016-06-13 23:44:29 -0700

    Create general App

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,7 @@
+require('style!css!foundation-sites/dist/foundation.min.css');
+require('style!css!sass!./styles/app.scss');
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
@@ -6,23 +10,15 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
+import App from './components/app'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store= createStoreWithMiddleware(reducers);
 
-var HelloMessage = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h1>Napiyoruz ?</h1>
-      </div>
-    );
-  }
-});
 
 
-require('style!css!foundation-sites/dist/foundation.min.css');
-require('style!css!sass!./styles/app.scss');
+
+
 $(document).foundation();
 
 
@@ -30,7 +26,7 @@ $(document).foundation();
 ReactDOM.render(
   <Provider store={store}>
       <Router history={browserHistory}>
-        <Route path="/" component={HelloMessage}>
+        <Route path="/" component={App}>
         </Route>
       </Router>
 </Provider>,document.getElementById('app'));
--- public/bundle.js
+++ public/bundle.js
@@ -117,29 +117,18 @@
 
 	var _reducers2 = _interopRequireDefault(_reducers);
 
+	var _app = __webpack_require__(318);
+
+	var _app2 = _interopRequireDefault(_app);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	__webpack_require__(262);
+	__webpack_require__(266);
 
 	var createStoreWithMiddleware = (0, _redux.applyMiddleware)(_reduxThunk2.default)(_redux.createStore);
 	var store = createStoreWithMiddleware(_reducers2.default);
 
-	var HelloMessage = _react2.default.createClass({
-	  displayName: 'HelloMessage',
-
-	  render: function render() {
-	    return _react2.default.createElement(
-	      'div',
-	      null,
-	      _react2.default.createElement(
-	        'h1',
-	        null,
-	        'Napiyoruz ?'
-	      )
-	    );
-	  }
-	});
-
-	__webpack_require__(262);
-	__webpack_require__(266);
 	$(document).foundation();
 
 	_reactDom2.default.render(_react2.default.createElement(
@@ -148,7 +137,7 @@
 	  _react2.default.createElement(
 	    _reactRouter.Router,
 	    { history: _reactRouter.browserHistory },
-	    _react2.default.createElement(_reactRouter.Route, { path: '/', component: HelloMessage })
+	    _react2.default.createElement(_reactRouter.Route, { path: '/', component: _app2.default })
 	  )
 	), document.getElementById('app'));
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(7)))
@@ -31303,6 +31292,59 @@
 	};
 
 	exports.default = createPropTypes;
+
+/***/ },
+/* 318 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(8);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var App = function (_Component) {
+	  _inherits(App, _Component);
+
+	  function App() {
+	    _classCallCheck(this, App);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(App).apply(this, arguments));
+	  }
+
+	  _createClass(App, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'h1',
+	          null,
+	          'App Start'
+	        )
+	      );
+	    }
+	  }]);
+
+	  return App;
+	}(_react.Component);
+
+	exports.default = App;
 
 /***/ }
 /******/ ]);
